3e87a59c80f4540c1a94a82c7b0a08db9eef5c3d
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-29 21:39:27 -0400

    Starting over with class approach, may try factory functions later for more practice

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-function linkedList() {
-  const list = [];
-  let head = null;
-  return {
-    list,
-    append: function (value) {
-      let prevNode = this.list.length - 1;
-      if (head === null) {
-        head = node(value);
-        this.list.push(head);
-      } else {
-        let newNode = node(value);
-        this.list.push(newNode);
-        this.list[prevNode].nextNode = newNode.value;
-      }
-    },
-    // prepend: function (value) {
-    //   this.list.unshift(node(value, this));
-    // },
-  };
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
 }
 
-function node(value = null, nextNode = null) {
-  return {
-    value,
-    nextNode,
-  };
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value;
+    this.nextNode;
+  }
 }
-
-const myLinkedList = linkedList();
-
-myLinkedList.append(500);
-myLinkedList.append(250);
-myLinkedList.append(700);
-console.log(myLinkedList);
